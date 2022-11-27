24510a6f51599417ac73d3255c7c99be020264aa
Jim Hays <jim@emporatitle.com>
2022-11-26 23:10:56 -0500

    Add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html'
+  ],
   theme: {
     extend: {},
   },
