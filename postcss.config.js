bea331c42fc6a75716740766785da80b3906af53
Jim Hays <jim@emporatitle.com>
2022-11-28 09:01:34 -0500

    Finish adding tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     plugins: [
-      require('postcss-import'),
-      require('tailwindcss'),
-      require('autoprefixer'),
-      ...(process.env.JEKYLL_ENV == "production"
-        ? [require('cssnano')({ preset: 'default' })]
-        : [])
+        require('postcss-import'),
+        require('tailwindcss')('./tailwind.config.js'),
+        require('autoprefixer'),
+//      ...(process.env.JEKYLL_ENV == "production"
+//        ? [require('cssnano')({ preset: 'default' })]
+//        : [])
     ]
-  };
+}
