3b08391ae409a7b644928d4b926c12896804f63c
calvinballing <calvinballing@users.noreply.github.com>
2022-11-30 04:39:28 +0000

    deploy: b987d0d0fa819015c542c53498f22500761105ad

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     plugins: [
-        require('postcss-import'),
+        require('postcss-import')({
+            path: ["css"]
+        }),
         require('tailwindcss')('./tailwind.config.js'),
         require('autoprefixer'),
 //      ...(process.env.JEKYLL_ENV == "production"
